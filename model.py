35d5d2475a4fc5264a0082c1ee3442401f3aaf6d
dolggul <yonunim@gmail.com>
2023-12-12 05:00:39 +0900

    feat: add return value called feature

--- model.py
+++ model.py
@@ -1,23 +1,28 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-import torchinfo
-from torchinfo import summary
+
 
 class CNNModel(nn.Module):
     def __init__(self):
         super().__init__()
-        
+
         # Define the layers
         self.conv1 = nn.Conv2d(in_channels=3, out_channels=32, kernel_size=3, padding=1)
-        self.conv2 = nn.Conv2d(in_channels=32, out_channels=64, kernel_size=3, padding=1)
-        self.conv3 = nn.Conv2d(in_channels=64, out_channels=128, kernel_size=3, padding=1)
-        self.conv4 = nn.Conv2d(in_channels=128, out_channels=256, kernel_size=3, padding=1)
+        self.conv2 = nn.Conv2d(
+            in_channels=32, out_channels=64, kernel_size=3, padding=1
+        )
+        self.conv3 = nn.Conv2d(
+            in_channels=64, out_channels=128, kernel_size=3, padding=1
+        )
+        self.conv4 = nn.Conv2d(
+            in_channels=128, out_channels=256, kernel_size=3, padding=1
+        )
         self.dropout = nn.Dropout(0.1)
-        self.fc1 = nn.Linear(256 * 8 * 8, 256) 
-        self.fc2 = nn.Linear(256, 128) 
-        self.fc3 = nn.Linear(128, 1) 
-        
+        self.fc1 = nn.Linear(256 * 8 * 8, 256)
+        self.fc2 = nn.Linear(256, 128)
+        self.fc3 = nn.Linear(128, 1)
+
     def forward(self, x):
         x = F.leaky_relu(self.conv1(x))
         x = F.leaky_relu(self.conv2(x))
@@ -26,9 +31,10 @@ class CNNModel(nn.Module):
         x = F.leaky_relu(self.conv4(x))
         x = F.max_pool2d(x, kernel_size=2)
         x = x.view(x.size(0), -1)
+        feature = x
         x = F.leaky_relu(self.fc1(x))
         x = self.dropout(x)
         x = F.leaky_relu(self.fc2(x))
         x = self.dropout(x)
         x = torch.sigmoid(self.fc3(x))
-        return x
+        return x, feature
