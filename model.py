53a486dbfb157bedcd20a56b5b6cc1eae38f5017
dolggul <yonunim@gmail.com>
2023-12-12 03:26:45 +0900

    refactore: restruct model forward func

--- model.py
+++ model.py
@@ -19,16 +19,18 @@ class CNNModel(nn.Module):
         
     def forward(self, x):
         x = self.conv1(x)
-        x = F.relu(x)
+        x = F.leaky_relu(x)
         x = F.max_pool2d(x, kernel_size=2)
         x = self.conv2(x)
-        x = F.relu(x)
+        x = F.leaky_relu(x)
         x = F.max_pool2d(x, kernel_size=2)
         x = self.conv3(x)
-        x = F.relu(x)
+        x = F.leaky_relu(x)
         x = self.dropout1(x)
         x = x.view(x.size(0), -1)
-        x = F.relu(self.fc1(x))
+        x = self.fc1(x)
+        x = F.leaky_relu(x)
         x = self.dropout2(x)
-        x = torch.sigmoid(self.fc2(x))
+        x = self.fc2(x)
+        x = torch.sigmoid(x)
         return x
