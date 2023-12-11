75c47f9abd6863bca574f10a7ccec3aa28b90175
dolggul <yonunim@gmail.com>
2023-12-12 05:04:04 +0900

    feat: add _ paramter in train.py

--- train.py
+++ train.py
@@ -1,24 +1,21 @@
 import torch
 import torch.nn as nn
 import torch.optim as optim
-import matplotlib.pyplot as plt
-from model import CNNModel
-from data_loader import get_loaders
 from sklearn.metrics import f1_score
 
 
-
-def train_model(model, train_loader, val_loader, num_epochs, learning_rate, device, save_interval=1):
-    
+def train_model(
+    model, train_loader, val_loader, num_epochs, learning_rate, device, save_interval=1
+):
     # loss func, optimizer
     criterion = nn.BCELoss()
-    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay = 1e-5)
+    optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=1e-5)
 
     # store value
     train_losses = []
     val_losses = []
     f1_scores = []
-    best_loss = float('inf')
+    best_loss = float("inf")
 
     for epoch in range(num_epochs):
         # Training
@@ -29,7 +26,7 @@ def train_model(model, train_loader, val_loader, num_epochs, learning_rate, devi
             labels = labels.to(device)
 
             optimizer.zero_grad()
-            outputs = model(images)
+            outputs, _ = model(images)
             loss = criterion(outputs, labels.float().unsqueeze(1))
             loss.backward()
             optimizer.step()
@@ -44,7 +41,7 @@ def train_model(model, train_loader, val_loader, num_epochs, learning_rate, devi
         with torch.no_grad():
             for images, labels in val_loader:
                 images, labels = images.to(device), labels.to(device)
-                outputs = model(images)
+                outputs, _ = model(images)
                 loss = criterion(outputs, labels.float().unsqueeze(1))
                 valid_loss += loss.item() * images.size(0)
                 predicted = (outputs > 0.5).float().squeeze()
@@ -52,17 +49,19 @@ def train_model(model, train_loader, val_loader, num_epochs, learning_rate, devi
                 valid_targets.extend(labels.cpu().numpy())
 
             valid_loss = valid_loss / len(val_loader.dataset)
-            f1 = f1_score(valid_targets, valid_preds, average='binary')
+            f1 = f1_score(valid_targets, valid_preds, average="binary")
 
-        print(f"Epoch {epoch+1}/{num_epochs}, \
+        print(
+            f"Epoch {epoch+1}/{num_epochs}, \
             Train Loss: {train_loss:.4f}, \
             Valid Loss: {valid_loss:.4f}, \
-            F1 Score: {f1:.4f}")
-        
+            F1 Score: {f1:.4f}"
+        )
+
         if epoch % save_interval == 0:
             if valid_loss < best_loss:
                 best_loss = valid_loss
-                torch.save(model.state_dict(), f'model_checkpoint.pth')
+                torch.save(model.state_dict(), "model_checkpoint.pth")
 
     train_losses.append(train_loss)
     val_losses.append(valid_loss)
