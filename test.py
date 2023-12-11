caf2053c31571db6a257ae520c315404dd614c51
dolggul <yonunim@gmail.com>
2023-12-12 05:05:15 +0900

    refactor: add _ paremter in test.py

--- test.py
+++ test.py
@@ -1,12 +1,11 @@
 import torch
 import torch.nn as nn
-from model import CNNModel
-from data_loader import get_loaders
 from sklearn.metrics import f1_score
+
 
 def test_model(model, test_loader, device):
     # Load the model
-    model.load_state_dict(torch.load('model_checkpoint.pth'))
+    model.load_state_dict(torch.load("model_checkpoint.pth"))
     model.eval()
 
     # criterion
@@ -19,7 +18,7 @@ def test_model(model, test_loader, device):
     with torch.no_grad():
         for images, labels in test_loader:
             images, labels = images.to(device), labels.to(device)
-            outputs = model(images)
+            outputs, _ = model(images)
             loss = criterion(outputs, labels.float().unsqueeze(1))
             test_loss += loss.item() * images.size(0)
             predicted = (outputs > 0.5).float().squeeze()
@@ -27,6 +26,6 @@ def test_model(model, test_loader, device):
             test_targets.extend(labels.cpu().numpy())
 
     test_loss = test_loss / len(test_loader.dataset)
-    f1 = f1_score(test_targets, test_preds, average='binary')
+    f1 = f1_score(test_targets, test_preds, average="binary")
 
     print(f"Test Loss: {test_loss:.4f}, F1 Score: {f1:.4f}")
