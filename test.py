c149c33a65a3c1dd754312eb615449ca77b0b588
dolggul <yonunim@gmail.com>
2023-12-13 22:35:52 +0900

    feat: add test_xgb func in test.py

--- test.py
+++ test.py
@@ -1,6 +1,11 @@
+import numpy as np
 import torch
 import torch.nn as nn
+from extract_feature import extract_feature
+from sklearn.metrics import accuracy_score
 from sklearn.metrics import f1_score
+from sklearn.metrics import precision_score
+from sklearn.metrics import recall_score
 
 
 def test_model(model, test_loader, device):
@@ -29,3 +34,29 @@ def test_model(model, test_loader, device):
     f1 = f1_score(test_targets, test_preds, average="binary")
 
     print(f"Test Loss: {test_loss:.4f}, F1 Score: {f1:.4f}")
+
+
+def test_xgb(model, xgb_model, test_loader, device, model_path):
+    # Load model
+    xgb_model.load_model(model_path)
+
+    # extract
+    features_test, labels_test = extract_feature(model, test_loader, device)
+
+    # list to numpy
+    features_test = np.array(features_test)
+    labels_test = np.array(labels_test)
+
+    # test
+    predictions = xgb_model.predict(features_test)
+
+    # matrics
+    accuracy = accuracy_score(labels_test, predictions)
+    precision = precision_score(labels_test, predictions)
+    recall = recall_score(labels_test, predictions)
+    f1 = f1_score(labels_test, predictions)
+
+    print(f"Accuracy: {accuracy:.4f}%")
+    print(f"Precision: {precision:.4f}%")
+    print(f"Recall: {recall:.4f}%")
+    print(f"F1 Score: {f1:.4f}%")
